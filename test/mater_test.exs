75291b5eccddf9288bc6336342693e9a862723a6
WLSF <willianluigii@gmail.com>
2022-09-24 20:45:46 -0300

    :test_tube: Add first test case

--- test/mater_test.exs
+++ test/mater_test.exs
@@ -2,7 +2,30 @@ defmodule MaterTest do
   use ExUnit.Case
   doctest Mater
 
-  test "greets the world" do
-    assert Mater.hello() == :world
+  defmodule Normal.Github do
+    alias Mater
+
+    @endpoint ~s(https://api.github.com/graphql)
+    @auth [{"Authorization", "Bearer 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"}]
+
+    def fetch_repo(owner, name) do
+      query = """
+        query {
+          repository(owner: "#{owner}", name: "#{name}") {
+            id,
+            name,
+            pushedAt
+          }
+        }
+        """
+
+      Mater.call(@endpoint, query, @auth)
+    end
+  end
+
+  describe "Mater basics" do
+    test "fetch data from a Github repo by owner and name" do
+      assert %{"data" => %{"repository" => %{"name" => "mater"}}} = Normal.Github.fetch_repo("WLSF", "mater")
+    end
   end
 end
