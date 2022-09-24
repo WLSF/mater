9a4b4c43a1c8edd5c72084cf5367eea25e874675
WLSF <willianluigii@gmail.com>
2022-09-24 20:47:09 -0300

    :memo: Add docs for call/3

--- lib/mater.ex
+++ lib/mater.ex
@@ -4,15 +4,17 @@ defmodule Mater do
   """
 
   @doc """
-  Hello world.
+  Executes a HTTP request for a GraphQL Endpoint with the given:
+    * endpoint
+    * query (containing: query, variables, operationName)
+    * opts (simples keyword list just in case you need some auth)
 
   ## Examples
 
-      iex> Mater.hello()
-      :world
+      iex> Mater.call("https://api.github.com/graphql", %{query: "query {repository...}"}, [{"Authorization", "Bearer ..."}])
 
   """
-  def hello do
-    :world
+  @spec call(String.t(), map(), Keyword.t()) :: {:ok, map()} | {:error, map()}
+  def call(endpoint, query, opts) do
   end
 end
